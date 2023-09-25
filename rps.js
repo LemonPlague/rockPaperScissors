4305a652c3be467d5af8ce222d7676de37e2172d
Richard <moorerichardplease@gmail.com>
2023-09-25 17:49:45 +0900

    Complete getPlayerChoice & getComputerChoice functions.

--- rps.js
+++ rps.js
@@ -4,16 +4,6 @@ const playerChoice = getPlayerChoice();
 const computerChoice = getComputerChoice();
 
 
-
-
-//function to get the computer's choice. This function will randomly select a number
-//between one and three, which are then returned as 
-//1 - rock, 2 - paper, 3 - scissors. Finally, the computer choice will also be
-//printed in the console so the player can also feel satisfied about the results.
-function getComputerChoice () {
-    
-}
-
 //function to get the player's choice via prompt. Player will type their choice
 //which is received as a string and returned.
 function getPlayerChoice() {
@@ -21,3 +11,28 @@ function getPlayerChoice() {
     console.log(`Player throws ${x}`)
     return x;
 }
+
+//function to get the computer's choice. This function will randomly select a number
+//between one and three, which are then returned as 
+//1 - rock, 2 - paper, 3 - scissors. Finally, the computer choice will also be
+//printed in the console so the player can also feel satisfied about the results.
+function getComputerChoice () {
+    let x = Math.floor((Math.random() * 3) + 1);
+    switch (x) {
+        case 1:
+            return "rock"
+            console.log(`Computer throws rock`);
+            break;
+
+        case 2:
+            return "paper"
+            console.log(`Computer throws paper.`);
+            break;
+
+        case 3:
+            return "scissors"
+            console.log(`Computer throws scissors`);
+            break;
+    }
+}
+
