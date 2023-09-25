757fb1286254a1327f546762abdade53fc23df85
Richard <moorerichardplease@gmail.com>
2023-09-25 18:08:09 +0900

    Complete the playRound function with comments

--- rps.js
+++ rps.js
@@ -3,6 +3,7 @@
 const playerChoice = getPlayerChoice();
 const computerChoice = getComputerChoice();
 
+playRound();
 
 //function to get the player's choice via prompt. Player will type their choice
 //which is received as a string and returned.
@@ -20,19 +21,71 @@ function getComputerChoice () {
     let x = Math.floor((Math.random() * 3) + 1);
     switch (x) {
         case 1:
-            return "rock"
-            console.log(`Computer throws rock`);
-            break;
+                return "rock"
+                console.log(`Computer throws rock`);
+                break;
 
         case 2:
-            return "paper"
-            console.log(`Computer throws paper.`);
-            break;
+                return "paper"
+                console.log(`Computer throws paper.`);
+                break;
 
         case 3:
-            return "scissors"
-            console.log(`Computer throws scissors`);
-            break;
+                return "scissors"
+                console.log(`Computer throws scissors`);
+                break;
     }
 }
 
+//This function will play a single round of Rock Paper Scissors
+//with the results being logged in the console. The function utilizes
+//two Switch Conditionals, 3 potential outcomes for each potential
+//choice the player can make.
+function playRound(computerChoice, playerChoice) {
+    switch (playerChoice)  {
+        case "rock":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("Aiko")
+                    break;
+                
+                case "paper":
+                    console.log("You Lose")
+                    break;
+
+                case "scissors":
+                    console.log("You Win")
+                    break;
+            }
+        
+        case "paper":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("You Win")
+                    break;
+                
+                case "paper":
+                    console.log("Aiko")
+                    break;
+
+                case "scissors":
+                    console.log("You Lose")
+                    break;
+            }
+
+        case "scissors":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("You Lose")
+                    break;
+                
+                case "paper":
+                    console.log("You Win")
+                    break;
+
+                case "scissors":
+                    console.log("Aiko")
+                    break;
+            }
+    }
+}
